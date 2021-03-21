55e993d9564759e1a6ad7307a7d7bb71f8a58ad0
fizzx <kabeerf96@gmail.com>
2021-03-21 20:43:45 +0000

    Removed unnecessary comments

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -27,22 +27,19 @@ func openMySQLDB(dbUser, dbPass, ipAddr, portNo, dbName string) (*sql.DB, error)
 }
 
 func getDatabaseCredentials(jsonCredFile string) (string, string) {
-	// Open our jsonFile
+
 	jsonFile, err := os.Open(jsonCredFile)
-	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("Successfully Opened credentials.json")
-	// defer the closing of our jsonFile so that we can parse it later on
+
 	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	// initialize our Users array
+
 	var credentials Credentials
 
-	// unmarshal our byteArray which contains our
-	// jsonFile's content into 'users' which we defined above
 	err = json.Unmarshal(byteValue, &credentials)
 	if err != nil {
 		fmt.Println(err)
